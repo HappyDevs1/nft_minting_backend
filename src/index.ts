e1f8fb0c873c54565b1cc77950c33525f359466b
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-02-15 21:51:43 +0200

    build: :tada: setting up the express server with typescript and connection to mongodb atlas

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose"
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+const mongo = process.env.MONGO_URL;
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+mongoose.connect(mongo as string)
+.then(() => {
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+})
+.catch((error) => {
+  console.log("Failed to connect to datatabase: ", error)
+})
